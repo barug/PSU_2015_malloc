5a745363294abadfb47fb809a701c98aec0b136d
Erwan Dupard <erwan.dupard@epitech.eu>
2016-01-28 14:17:57 +0100

    Comments to header

--- ressources.h
+++ ressources.h
@@ -5,13 +5,19 @@
 ** Login   <dupard_e@epitech.net>
 ** 
 ** Started on  Thu Jan 28 10:25:45 2016 Erwan Dupard
-** Last update Thu Jan 28 11:18:55 2016 Erwan Dupard
+// Last update Thu Jan 28 14:17:45 2016 Erwan Dupard
 */
 
 #ifndef RESSOURCES_H_
 # define RESSOURCES_H_
 
 # include <unistd.h>
+
+/*
+ * Align 32bits int
+ */
+
+# define align4(x) (((((x) - 1) >> 2) << 2) + 4)
 
 /*
  * Malloc implementation
@@ -37,12 +43,14 @@ void			show_alloc_mem();
 /*
  * Definition of memory block (set in malloc , used for free)
  * We can call this "Meta-data"
+ * 
+ * Heap representation
  */
 typedef struct		s_block
 {
-  size_t		size;
-  struct s_block	*next;
-  int			free;
+  size_t		size; /* size of allocated block */
+  struct s_block	*next; /* next memory block */
+  int			free; /* Is the pointed byte is ready to be allocated */
 }			t_block;
 
 #endif /* ! RESSOURCES_H_ */
