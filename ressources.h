61a7da426bc7cb1974509161a9cdd2263f704fcf
Erwan Dupard <erwan.dupard@epitech.eu>
2016-01-28 10:27:09 +0100

    Adding some comments in header file

--- ressources.h
+++ ressources.h
@@ -1,22 +1,46 @@
-//
-// ressources.h for malloc in /home/dupard_e/rendus/PSU_2015_malloc
-// 
-// Made by Erwan Dupard
-// Login   <dupard_e@epitech.net>
-// 
-// Started on  Tue Jan 26 23:51:38 2016 Erwan Dupard
-// Last update Wed Jan 27 00:20:43 2016 Erwan Dupard
-//
+/*
+** ressources.h for malloc in /home/dupard_e/rendus/PSU_2015_malloc
+** 
+** Made by Erwan Dupard
+** Login   <dupard_e@epitech.net>
+** 
+** Started on  Thu Jan 28 10:25:45 2016 Erwan Dupard
+** Last update Thu Jan 28 10:26:50 2016 Erwan Dupard
+*/
 
 #ifndef RESSOURCES_H_
 # define RESSOURCES_H_
 
 # include <unistd.h>
 
-void		*malloc(size_t size);
-void		*realloc(void *ptr, size_t size);
-void		free(void *ptr);
-void		show_alloc_mem();
+/*
+ * Malloc implementation
+ */
+void			*malloc(size_t size);
 
+/*
+ * Realloc implementation
+ */
+void			*realloc(void *ptr, size_t size);
+
+/*
+ * Free implementation
+ */
+void			free(void *ptr);
+
+/*
+ * User readable memory dump
+ */
+void			show_alloc_mem();
+
+
+/*
+ * Definition of memory block (set in malloc , used for free)
+ */
+typedef struct		s_block
+{
+  size_t		size;
+  struct s_block	*next;
+}			t_block;
 
 #endif /* ! RESSOURCES_H_ */
