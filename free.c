bb85cff7f7d6a9ce142980f4c31455f80a7c6fe5
Erwan Dupard <erwan.dupard@epitech.eu>
2016-01-29 15:24:14 +0100

    That last commit message about silly mistakes pales in comparision to this one

--- free.c
+++ free.c
@@ -5,16 +5,14 @@
 ** Login   <dupard_e@epitech.net>
 ** 
 ** Started on  Fri Jan 29 14:24:02 2016 Erwan Dupard
-** Last update Fri Jan 29 14:46:12 2016 Erwan Dupard
+** Last update Fri Jan 29 15:24:12 2016 Erwan Dupard
 */
 
 #include "ressources.h"
 
 static t_block		*get_elem_by_ptr(void *ptr)
 {
-  //  return ((t_block *)ptr - NODE_SIZE);
-  (void)ptr;
-  return (NULL);
+  return ((t_block *)ptr - NODE_SIZE);
 }
 
 void			free(void *ptr)
